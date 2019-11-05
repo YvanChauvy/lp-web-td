16c561bf94fa0fcb72e8bf3121a8f93ed1a70cf6
Yvan CHAUVY <ychauvy@gmail.com>
2019-11-05 16:32:01 +0100

    [ADD] ajout du prepare de DB et fichier de base index et person

--- models/DB.php
+++ models/DB.php
@@ -9,10 +9,17 @@ class DB
     private $login = '193595' ;
     private $pwd = '123Azerty456' ;
     private $connection = null;
+    private $request = null;
 
     public function connect(){
         $this->connection = new PDO("mysql:host=$this->host:$this->port;dbname=$this->db",$this->login,$this->pwd);
         $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    }
+    public function prepare($statement,$placeholders = []){
+        $this->request = $this->connection->prepare($statement);
+        foreach ($placeholders as $key => $value){
+            $this->request->bindParam(":$key",$value);
+        }
     }
 
 
---
+++ models/Person.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class person
+{
+    private $id;
+    private $lastName;
+    private $firstName;
+    private $birthDate;
+    private $biography;
+}
