30af2c06be572c9fc57edbcfcecff78534549b49
Yvan CHAUVY <ychauvy@gmail.com>
2019-11-13 16:48:31 +0100

    [UPDATE] mise en place de la gestions des acces et redirections

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+$url = $_SERVER['REQUEST_URI'];
+try {
+
+    if (isset($url)) {
+        $urlContents = explode('/', filter_var($url, FILTER_SANITIZE_URL));
+        array_shift($urlContents);
+        $controller = ucfirst(strtolower($urlContents[0]));
+        $controllerClass = $controller . 'Controller';
+        $controllerFile = dirname(__DIR__) . '/controllers/' . $controllerClass;
+        if (file_exists($controllerFile)) {
+            require($controllerFile);
+            $this->_controller = new $controllerClass($url);
+        } else {
+            throw new Exception('Page introuvable');
+        }
+    } else //index
+    {
+        require_once dirname(__DIR__) . '/controllers/HomeController.php';
+        $this->_controller = new HomeController(array('index'));
+    }
+
+}catch(Exception $e)
+{
+    $error = $e->getMessage();
+    $this->_view = new View('Error', 'Erreur | A.P.P.M',array('error' => $error));
+}
--- models/DB.php
+++ models/DB.php
@@ -3,30 +3,62 @@
 
 class DB
 {
-    private $host = 'mysql-yvan-chauvy-lp-web.alwaysdata.net' ;
-    private $port=3306;
-    private $db = 'yvan-chauvy-lp-web_td' ;
-    private $login = '193595' ;
-    private $pwd = '123Azerty456' ;
+    private $host = 'mysql-yvan-chauvy-lp-web.alwaysdata.net';
+    private $port = 3306;
+    private $db = 'yvan-chauvy-lp-web_td';
+    private $login = '193595';
+    private $pwd = '123Azerty456';
     private $connection = null;
     private $request = null;
 
-    public function connect(){
-        $this->connection = new PDO("mysql:host=$this->host:$this->port;dbname=$this->db",$this->login,$this->pwd);
-        $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    }
-    public function prepare($statement,$placeholders = []){
-        $this->request = $this->connection->prepare($statement);
-        foreach ($placeholders as $key => $value){
-            $this->request->bindParam(":$key",$value);
+    public function connect()
+    {
+        try {
+            $this->connection = new PDO("mysql:host=$this->host:$this->port;dbname=$this->db", $this->login, $this->pwd);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        } catch (PDOException $e) {
+            throw $e;
         }
     }
 
-    public function query(){
-        $this->request->execute();
+    public function prepare($statement, $placeholders = [])
+    {
+        try {
+            $this->request = $this->connection->prepare($statement);
+            foreach ($placeholders as $key => $value) {
+                $this->request->bindParam(":$key", $value);
+            }
+        } catch (PDOException $e) {
+            throw $e;
+        }
     }
 
-    public function fetch($fetchType = PDO::FETCH_ASSOC){
-       return $this->request->fetch($fetchType);
+    public function query()
+    {
+        try {
+            $this->request->execute();
+        } catch (PDOException $e) {
+            ?>
+            <p><strong>Caught <?= get_class($e) ?></strong>: <?= $e->getMessage() ?></p>
+            <p>Query <?= $query ?></p>
+            <p>Query parameters:
+            <pre><?= var_export($queryParameters, true) ?></pre></p>
+            <p>Exception trace:
+            <pre><?= $e->getTraceAsString() ?></pre></p>
+            <?php
+            die();
+        }
+    }
+
+
+    public
+    function fetch($fetchType = PDO::FETCH_ASSOC)
+    {
+        try {
+            return $this->request->fetch($fetchType);
+        } catch (PDOException $e) {
+            throw $e;
+        }
     }
 }
