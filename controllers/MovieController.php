3feb58c8fe50cfb3d1a91c0919850a88a2ff7f12
Yvan CHAUVY <ychauvy@gmail.com>
2019-11-15 11:45:18 +0100

    [ADD] debut ajout du film avatar

--- controllers/MovieController.php
+++ controllers/MovieController.php
@@ -3,5 +3,14 @@
 
 class MovieController extends Controller
 {
+    public function index()
+    {
+        utils::getBlock('views/header.html');
+        utils::getBlock('views/nav.php');
+        utils::getBlock('views/index.html');
+    }
 
+    public function displayMovie(){
+
+    }
 }
