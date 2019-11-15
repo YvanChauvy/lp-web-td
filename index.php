68f273c2eec8ae9693d65461dc2693a75ab7cf73
Yvan CHAUVY <ychauvy@gmail.com>
2019-11-15 09:44:40 +0100

    [UPDATE] Gestion des redirection

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
+require 'utils.php';
+
 $url = $_SERVER['REQUEST_URI'];
+var_dump($url);
 try {
 
-    if (isset($url)) {
+    if (isset($url) && $url != '/') {
         $urlContents = explode('/', filter_var($url, FILTER_SANITIZE_URL));
         array_shift($urlContents);
-        $controller = ucfirst(strtolower($urlContents[0]));
-        $controllerClass = $controller . 'Controller';
+        $controller = ucfirst(strtolower($urlContents));
+        $controllerClass = $controller . 'Controller.php';
         $controllerFile = dirname(__DIR__) . '/controllers/' . $controllerClass;
         if (file_exists($controllerFile)) {
             require($controllerFile);
-            $this->_controller = new $controllerClass($url);
+            $this->_controller = new $controllerClass($urlContents);
         } else {
             throw new Exception('Page introuvable');
         }
@@ -23,5 +26,4 @@ try {
 }catch(Exception $e)
 {
     $error = $e->getMessage();
-    $this->_view = new View('Error', 'Erreur | A.P.P.M',array('error' => $error));
 }
