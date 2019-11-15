b0bbcc373558e90843e1fa732f61fc59c3dfa623
Yvan CHAUVY <ychauvy@gmail.com>
2019-11-15 10:00:10 +0100

    [UPDATE] changement des chemins

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 require 'utils.php';
+require __DIR__ . '/controllers/Controller.php';
 
 $url = $_SERVER['REQUEST_URI'];
-var_dump($url);
 try {
 
     if (isset($url) && $url != '/') {
         $urlContents = explode('/', filter_var($url, FILTER_SANITIZE_URL));
         array_shift($urlContents);
-        $controller = ucfirst(strtolower($urlContents));
+        $controller = ucfirst(strtolower($urlContents[0]));
         $controllerClass = $controller . 'Controller.php';
-        $controllerFile = dirname(__DIR__) . '/controllers/' . $controllerClass;
+        $controllerFile = __DIR__ . '/controllers/' . $controllerClass;
         if (file_exists($controllerFile)) {
             require($controllerFile);
             $this->_controller = new $controllerClass($urlContents);
@@ -19,8 +19,8 @@ try {
         }
     } else //index
     {
-        require_once dirname(__DIR__) . '/controllers/HomeController.php';
-        $this->_controller = new HomeController(array('index'));
+        require_once __DIR__ . '/controllers/HomeController.php';
+        new HomeController(array('index'));
     }
 
 }catch(Exception $e)
