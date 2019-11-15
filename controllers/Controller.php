aaa7e185b7077b178a40814ce0eb0800c3d2c848
Yvan CHAUVY <ychauvy@gmail.com>
2019-11-15 10:00:34 +0100

    [ADD] creation de l'index

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -5,12 +5,6 @@ abstract class Controller
 {
     public function __construct($url)
     {
-        if(is_numeric($url[1]))
-        {
-            $params = array_splice($url, 1, sizeof($url));
-            $this->show($params);
-            return;
-        }
         $method = $url[1];
         $params = array_splice($url, 2, sizeof($url));
         if(is_null($method))
@@ -20,5 +14,7 @@ abstract class Controller
         else
             throw new Exception('Page introuvable');
     }
+
+    protected abstract function index();
 
 }
--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,4 +1,10 @@
 <?php
 class HomeController extends Controller {
 
+    protected function index()
+    {
+        utils::getBlock('views/header.html');
+        utils::getBlock('views/nav.php');
+        utils::getBlock('views/index.html');
+    }
 }
